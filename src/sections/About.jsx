b1038f21a57756f1005498b08fa5aef409c2812a
Matthew <matttfung@gmail.com>
2025-05-10 17:27:39 +0800

    fixed some animations, added project animations

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Description from "@/components/UI/Description";
 import ExperienceLabel from "@/components/ExperienceLabel";
 import ParagraphTitle from "@/components/UI/ParagraphTitle";
@@ -5,6 +6,7 @@ import TechCard from "@/components/TechCard";
 import careerList from "@/data/career";
 import ExperienceCard from "@/components/ExperienceCard";
 import ReavealEffect from "@/components/UI/RevealEffect";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
@@ -14,45 +16,49 @@ const About = () => {
     >
       <div className="w-[80%] md:w-[75%] lg:w-[60%] xl:w-[50%] flex flex-col lg:flex-row gap-2 z-20">
         {/* About Me */}
-        <ReavealEffect initialX={-100}>
-          <div className="lg:w-1/2 flex flex-col gap-4">
-            <ParagraphTitle title="About Me" />
-            <Description
-              text="I graduated from PolyU in 2024 as a Computing Major.
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.2 }}
+          className="lg:w-1/2 flex flex-col gap-4"
+        >
+          <ParagraphTitle title="About Me" />
+          <Description
+            text="I graduated from PolyU in 2024 as a Computing Major.
             I'm now working as a Software Engineer in Hong Kong specializing in Web Devlopment. My
             duties are to bring client's specifications and requirements to life
             using HTML and CSS. I've helped created an AI Chatbot SaaS platform
             using ASP.net."
+          />
+          <div className="flex gap-2">
+            <ExperienceLabel
+              years={1}
+              qualification={"Year Fulltime Experience"}
             />
-            <div className="flex gap-2">
-              <ExperienceLabel
-                years={1}
-                qualification={"Year Fulltime Experience"}
-              />
-              <ExperienceLabel
-                years={1}
-                qualification={"Year Part-time Experience"}
-              />
-            </div>
+            <ExperienceLabel
+              years={1}
+              qualification={"Year Part-time Experience"}
+            />
           </div>
-        </ReavealEffect>
+        </motion.div>
 
         {/* Skills */}
-        <ReavealEffect initialX={-100} delay={0.2}>
-          <div className="lg:w-1/2 mt-4 md:mt-0 flex flex-col">
-            <span className="text-[32px] font-semibold mb-4">
-              My Core Skills
-            </span>
-            <div className="grid grid-cols-3 gap-2">
-              <TechCard src={"/tech/html.png"} />
-              <TechCard src={"/tech/css.png"} />
-              <TechCard src={"/tech/react.png"} />
-              <TechCard src={"/tech/tailwind.png"} />
-              <TechCard src={"/tech/framer-motion.png"} />
-              <TechCard src={"/tech/dotnet.png"} />
-            </div>
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.2 }}
+          className="lg:w-1/2 mt-4 md:mt-0 flex flex-col"
+        >
+          <span className="text-[32px] font-semibold mb-4">My Core Skills</span>
+          <div className="grid grid-cols-3 gap-2">
+            <TechCard src={"/tech/html.png"} />
+            <TechCard src={"/tech/css.png"} />
+            <TechCard src={"/tech/react.png"} />
+            <TechCard src={"/tech/tailwind.png"} />
+            <TechCard src={"/tech/framer-motion.png"} />
+            <TechCard src={"/tech/dotnet.png"} />
           </div>
-        </ReavealEffect>
+        </motion.div>
       </div>
 
       <div
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -3,7 +3,7 @@ import { ProjectBlock } from "@/components/UI/Block";
 import { useState } from "react";
 import projectList from "@/data/project";
 import ProjectModal from "@/components/ProjectModal";
-import ReavealEffect from "@/components/UI/RevealEffect";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   const [open, setOpen] = useState(false);
@@ -19,7 +19,12 @@ const Projects = () => {
       id="projects"
       className="w-full md:h-[60vh] py-8 md:py-0 flex justify-center items-center bg-foreground relative"
     >
-      <div className="w-[90%] md:w-[70%] flex flex-col md:flex-row items-center gap-2 z-20">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0 }}
+        className="w-[90%] md:w-[70%] flex flex-col md:flex-row items-center gap-2 z-20"
+      >
         {/* Project Bento */}
         <div className="bg-foreground order-2 md:order-1 w-full md:w-1/2 flex justify-center items-center border border-zinc-600 rounded-lg p-4">
           <div className="grid grid-cols-12 grid-flow-dense gap-4">
@@ -37,13 +42,18 @@ const Projects = () => {
         </div>
 
         {/* Section Title */}
-        <div className="order-1 md:order-2 w-full md:w-1/2 flex flex-col md:justify-center px-5 mb-2 md:mb-0">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0 }}
+          className="order-1 md:order-2 w-full md:w-1/2 flex flex-col md:justify-center px-5 mb-2 md:mb-0"
+        >
           <span className="text-[96px] text-highlight w-fit ">Projects</span>
           <span className="text-[22px]">
             These are the projects that I've helped build
           </span>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
 
       {/* Top right corner Decor*/}
       <img
